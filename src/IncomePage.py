22e9c29033cc460aa758c50b4e8a8ed1db33bf74
neherh <helmutneher@gmail.com>
2018-11-07 21:33:25 -0500

    created widgets files to incorporate repeated code. Begun data layout for income/expenses...not finished; needs to fix data input

--- src/IncomePage.py
+++ src/IncomePage.py
@@ -1,18 +1,43 @@
 #!/usr/bin/kivy
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.uix.widget import Widget
 
 # from accordion_edit import Accordion
 import os
 # import sqlite3
 
+from Widgets import IncomeListItem, MutableTextInput
+
 class IncomeScreen(Screen):
+	"""
+	TODO:
+		- test to store information
+		- test ot load information
+		- format fields
+	"""
 	sessionStorage = ObjectProperty()
 	welcomeLabel = ObjectProperty()
+	data = ObjectProperty()
+
+	data_income = ListProperty() # temp
 
 	def set_user_information(self):
+
+		# welcome fields
 		self.welcomeLabel.text = str(self.sessionStorage.caseType) + " Income"
+		self.data.text = str(self.sessionStorage.baseFilepath)
+
+		# extract data if needed
+
+		# TEST: submit 
+
+	def args_converter(self, row_index, item):
+		return {
+            'note_index': row_index,
+            'note_type': item['type'],
+            'note_content': item['content'],
+            'note_title': item['title']}
 
 	def create_future_case(self):
 		# self.sessionStorage.caseType = "Future"
---
+++ src/Widgets.py
@@ -0,0 +1,52 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty, StringProperty, \
+        NumericProperty, BooleanProperty
+
+
+
+class MutableTextInput(FloatLayout):
+
+    text = StringProperty()
+    multiline = BooleanProperty(True)
+
+    def __init__(self, **kwargs):
+        super(MutableTextInput, self).__init__(**kwargs)
+        Clock.schedule_once(self.prepare, 0)
+
+    def prepare(self, *args):
+        self.w_textinput = self.ids.w_textinput.__self__
+        self.w_label = self.ids.w_label.__self__
+        self.view()
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos) and touch.is_double_tap:
+            self.edit()
+        return super(MutableTextInput, self).on_touch_down(touch)
+
+    def edit(self):
+        self.clear_widgets()
+        self.add_widget(self.w_textinput)
+        self.w_textinput.focus = True
+
+    def view(self):
+        self.clear_widgets()
+        if not self.text:
+            self.w_label.text = "Double tap/click to edit"
+        self.add_widget(self.w_label)
+
+    def check_focus_and_view(self, textinput):
+        if not textinput.focus:
+            self.text = textinput.text
+            self.view()
+
+class IncomeListItem(BoxLayout):
+
+    def __init__(self, **kwargs):
+        print(kwargs)
+        del kwargs['index']
+        super(IncomeListItem, self).__init__(**kwargs)
+    note_content = StringProperty()
+    note_title = StringProperty()
+    note_type = StringProperty()
+    note_index = NumericProperty()
